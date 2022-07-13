fa98841a0c12694755aeefca447c4bb3089e4656
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-12 19:31:52 -0500

    New bucket

--- forecast_data_prep_pipeline.py
+++ forecast_data_prep_pipeline.py
@@ -1,9 +1,15 @@
+import logging
 from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.utils import dates
+
+import urllib
+import google.auth.transport.requests
+import google.oauth2.id_token
 
 default_dag_args = {
     'start_date': dates.days_ago(0),
@@ -12,21 +18,42 @@ default_dag_args = {
 }
 
 with DAG(
-        'composer_sample_simple_greeting',
-        description='Hello World!',
-        schedule_interval=None,
+        'forecast_data_ingest_pipeline',
+        description='Run Forecast data ingestion pipeline',
+        schedule_interval=timedelta(hours=1),
         default_args=default_dag_args) as dag:
 
-    def greeting():
-        import logging
-        logging.info('Hello World!')
+    def hit_cloud_run():
+        service_url = 'https://forecast-data-loader-ldneeqwt7a-nw.a.run.app'
+        req = urllib.request.Request(service_url)
+
+        auth_req = google.auth.transport.requests.Request()
+        id_token = google.oauth2.id_token.fetch_id_token(auth_req, service_url)
+        req.add_header("Authorization", f"Bearer {id_token}")
+        response = urllib.request.urlopen(req)
+        logging.info('response=', response)
+        logging.info(response.read())
+
 
     hello_python = PythonOperator(
         task_id='hello',
-        python_callable=greeting)
+        python_callable=hit_cloud_run)
 
     goodbye_bash = BashOperator(
         task_id='bye',
         bash_command='echo Goodbye.')
 
-    hello_python >> goodbye_bash
+    hello_python 
+
+    # def greeting():
+    #     logging.info('Hello World!')
+    #
+    # hello_python = PythonOperator(
+    #     task_id='hello',
+    #     python_callable=greeting)
+    #
+    # goodbye_bash = BashOperator(
+    #     task_id='bye',
+    #     bash_command='echo Goodbye.')
+    #
+    # hello_python >> goodbye_bash
