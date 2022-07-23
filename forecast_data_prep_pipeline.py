e4a59afcc5213c1da04536142e98f5b70e223f71
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-23 14:49:36 +0100

    Moved DBT to new namespace

--- forecast_data_prep_pipeline.py
+++ forecast_data_prep_pipeline.py
@@ -58,7 +58,7 @@ with DAG(
 
     dbt_run = KubernetesPodOperator(
         task_id="dbt_run",
-        namespace='sa-gke-dbt',
+        namespace='dbt-pipelines',
         image='gcr.io/dt-patrick-project-dev/dbt-runner:latest',
         name="dbt-runner-pod",
         get_logs=True,
